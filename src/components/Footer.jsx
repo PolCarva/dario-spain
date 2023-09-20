48d2e027e1d2e86be6953eec6eae2f2afd70ba8c
PolCarva <pablocarvalhogimenez@gmail.com>
2023-09-20 12:42:41 -0300

    Added footer Responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,15 @@ import { Link as NavLink } from "react-scroll";
 const Footer = () => {
   return (
     <div className="mx-auto text-white p-normalize pt-10 pb-5 font-body">
-      <div className=" grid grid-cols-3 gap-32">
+      <div className=" grid grid-cols-1 md:grid-cols-3 gap-16  md:gap-32">
         <div className="flex flex-col gap-3">
-          <img src={images.logo} alt="Darío Spain" className="" />
+          <img src={images.logo} alt="Darío Spain" className="w-1/2 mx-auto md:w-full" />
           <span className=" text-center font-light italic text-sm">
             “Si puede ser escrito o pensado, puede ser fotografiado”
           </span>
         </div>
-        <div className="flex flex-col gap-3 items-center">
-          <ul>
+        <div className="flex flex-col gap-3 items-center text-xl md:text-base">
+          <ul className="grid grid-cols-2 gap-5 md:flex md:flex-col md:gap-1">
             <li className="font-light flex gap-4 items-center justify-start">
               <div className="w-0.5 h-0.5 bg-white rounded-full"></div>
               <NavLink
@@ -51,8 +51,8 @@ const Footer = () => {
           </ul>
         </div>
         <div className="flex flex-col gap-5">
-          <p className="text-center font-bold">Seguime y contáctanos</p>
-          <div className="flex justify-between">
+          <p className="text-center font-bold text-xl md:text-base">Seguime y contáctanos</p>
+          <div className="flex justify-between w-1/2 mx-auto md:w-full">
             <BsInstagram className="h-8 w-8" />
             <BsWhatsapp className="h-8 w-8" />
             <FiMail className="h-8 w-8" />
