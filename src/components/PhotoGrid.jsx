350c873d2b4812de6882dc249652f4df4075771e
PolCarva <pablocarvalhogimenez@gmail.com>
2023-10-02 18:53:15 -0300

    added lazy loading and small blurred img

--- src/components/PhotoGrid.jsx
+++ src/components/PhotoGrid.jsx
@@ -3,13 +3,19 @@ import { useLocation } from "react-router-dom";
 import Carousel3D from "./Carousel3D";
 import WhatsappFixedIcon from "../components/WhatsappFixedIcon";
 
+const getSmallImagePath = (mainImagePath) => {
+  const parts = mainImagePath.split("/");
+  parts.splice(-1, 0, "small");
+  return parts.join("/");
+};
 
 const PhotoGrid = ({ images }) => {
   const [isCarouselOpen, setIsCarouselOpen] = useState(false);
   const [activeImage, setActiveImage] = useState(null);
-
   const location = useLocation();
   const [category, setCategory] = useState("default");
+
+  const [loadedImages, setLoadedImages] = useState({});
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -35,23 +41,41 @@ const PhotoGrid = ({ images }) => {
     setIsCarouselOpen(true);
   };
 
+  const handleImageLoaded = (key) => {
+    setLoadedImages((prev) => ({
+      ...prev,
+      [key]: true,
+    }));
+  };
+
   return (
     <>
-    <WhatsappFixedIcon/>
+      <WhatsappFixedIcon />
       {isCarouselOpen && (
-        <Carousel3D activeImage={activeImage} images={images} onClose={() => {setIsCarouselOpen(false)}}/>
+        <Carousel3D
+          activeImage={activeImage}
+          images={images}
+          onClose={() => {
+            setIsCarouselOpen(false);
+          }}
+        />
       )}
       <div className="grid grid-cols-6 gap-4">
         {Object.keys(images).map((key, index) => {
           const colSpan = layout[index];
+          const isImageLoaded = loadedImages[key];
+          const mainImage = images[key];
+          const smallImage = getSmallImagePath(mainImage); // Obtener la ruta de la imagen small
+
           return (
             <img
-              loading="lazy"
+              onLoad={() => handleImageLoaded(key)}
               onClick={() => handleImageClick(key)}
               key={key}
-              src={images[key]}
+              loading="lazy"
+              src={isImageLoaded ? mainImage : smallImage}
               alt={`foto ${category}: ${key}`}
-              className={`w-full h-full !max-h-[80vh] object-cover cursor-pointer select-none hover:scale-110 border-2 border-transparent hover:border-white transition-all duration-300`}
+              className={`${!isImageLoaded ? "blur-sm" : "blur-0"} w-full h-full !max-h-[80vh] object-cover cursor-pointer select-none hover:scale-110 border-2 border-transparent hover:border-white transition-all duration-300`}
               style={{ gridColumn: `span ${colSpan}` }}
             />
           );
@@ -62,14 +86,3 @@ const PhotoGrid = ({ images }) => {
 };
 
 export default PhotoGrid;
-
-/* const categoryGridStyles = {
-  books: ["repeat(3, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(2, 1fr)"],
-  clientes: "repeat(3, 1fr)",
-  conciertos: "repeat(2, 1fr)",
-  desfiles: "repeat(3, 1fr)",
-  eventos: "repeat(3, 1fr)",
-  fotoperiodismo: "repeat(3, 1fr)",
-  moda: "repeat(3, 1fr)",
-  default: "repeat(3, 1fr)",
-}; */
